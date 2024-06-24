a71debb9a0070ea05f965ab59184752874c37113
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-06-24 16:55:06 -0500

    se desintegra del menu el login y logout

--- Frontend/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BaseComponent } from 'src/app/shared/core/base.component';
+import { UserService } from 'src/app/shared/services/user.service';
 
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
-    { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
-    { path: '/register', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
+  { path: '/dashboard', title: 'Dashboard', icon: 'ni-tv-2 text-primary', class: '' },
+  { path: '/icons', title: 'Icons', icon: 'ni-planet text-blue', class: '' },
+  { path: '/maps', title: 'Maps', icon: 'ni-pin-3 text-orange', class: '' },
+  { path: '/user-profile', title: 'User profile', icon: 'ni-single-02 text-yellow', class: '' },
+  { path: '/tables', title: 'Tables', icon: 'ni-bullet-list-67 text-red', class: '' },
+  // { path: '/login', title: 'Login', icon: 'ni-key-25 text-info', class: '' },
+  // { path: '/register', title: 'Register', icon: 'ni-circle-08 text-pink', class: '' }
 ];
 
 @Component({
@@ -22,17 +24,23 @@ export const ROUTES: RouteInfo[] = [
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent extends BaseComponent implements OnInit {
 
   public menuItems: any[];
   public isCollapsed = true;
 
-  constructor(private router: Router) { }
+  constructor(private route: Router, private userService: UserService) { super(); }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
-    this.router.events.subscribe((event) => {
+    this.route.events.subscribe((event) => {
       this.isCollapsed = true;
-   });
+    });
+  }
+
+  public async logout(): Promise<void> {
+    this.clearHistory();
+    this.route.navigate(['/login']);
+    await this.userService.logout();
   }
 }
