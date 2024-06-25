6630c93e1b06e1d6ad03afd12564877a9eccb567
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-06-25 14:08:13 -0500

    se corrigue manejo de angular json dado que daba problemas para crear paquetes

--- Frontend/src/app/pages/pages.module.ts
+++ Frontend/src/app/pages/pages.module.ts
@@ -12,6 +12,7 @@ import { IconsComponent } from './core/icons/icons.component';
 import { TablesComponent } from './core/tables/tables.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     UserProfileComponent,
     TablesComponent,
     IconsComponent,
-    MapsComponent
+    MapsComponent,
+    EmployeesListComponent
   ],
   imports: [
     CommonModule,
