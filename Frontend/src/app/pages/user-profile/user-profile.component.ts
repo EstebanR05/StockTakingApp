31ea0c36e4d9f408b2d65c139bb7662eca900c33
Steiner Herrera M. <57965977+steinerHD@users.noreply.github.com>
2024-06-24 22:33:09 -0500

    se agrega la funcionalidad de mostrar los datos del usuario en el perfil

--- Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/shared/core/base.component';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+export class UserProfileComponent extends BaseComponent implements OnInit {
+  public data: any = []
+  constructor( private userService: UserService) { super() }
 
   ngOnInit() {
+    this.getInfoUser();
+  }
+
+  async getInfoUser(): Promise<void> {
+    const response = await this.userService.getUser();
+    this.data = response;
   }
 
 }
