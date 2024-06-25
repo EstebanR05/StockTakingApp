12f63fcb66c6cb40007eb43fd0e10567f5f0fc99
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-06-25 12:49:36 -0500

    se integra mejoramiento de enrutamiento para pages

--- Frontend/src/app/pages/pages.module.ts
+++ Frontend/src/app/pages/pages.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { pagesRoutes } from './pages.routing';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ClipboardModule } from 'ngx-clipboard';
 
-import { PagesRoutingModule } from './pages-routing.module';
-
+import { MapsComponent } from './maps/maps.component';
+import { IconsComponent } from './icons/icons.component';
+import { TablesComponent } from './tables/tables.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    UserProfileComponent,
+    TablesComponent,
+    IconsComponent,
+    MapsComponent
+  ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    ClipboardModule,
+    RouterModule.forChild(pagesRoutes)
   ]
 })
 export class PagesModule { }
