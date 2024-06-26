1f702422716a3a0b045f5aa68b495e9c6aa8eaf5
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-06-26 15:38:56 -0500

    se integra nueva bd

--- Backend/bd/stocktaking.sql
+++ Backend/bd/stocktaking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2024 at 04:04 PM
+-- Generation Time: Jun 26, 2024 at 10:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -20,33 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `stocktaking`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employees`
---
-
-CREATE TABLE `employees` (
-  `id` int(11) NOT NULL,
-  `name` varchar(22) NOT NULL,
-  `lastName` varchar(22) DEFAULT NULL,
-  `email` varchar(22) NOT NULL,
-  `password` varchar(22) NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `staff` varchar(20) DEFAULT NULL,
-  `company` varchar(20) DEFAULT NULL,
-  `address` varchar(20) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `city` varchar(20) DEFAULT NULL,
-  `country` varchar(20) DEFAULT NULL,
-  `userInformation` varchar(20) DEFAULT NULL,
-  `postalCode` int(11) DEFAULT NULL,
-  `aboutMe` varchar(200) DEFAULT NULL,
-  `positionemployee` varchar(100) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -99,27 +72,22 @@ CREATE TABLE `users` (
   `country` varchar(20) DEFAULT NULL,
   `userInformation` varchar(120) DEFAULT NULL,
   `postalCode` int(11) DEFAULT NULL,
-  `aboutMe` varchar(200) DEFAULT NULL
+  `aboutMe` varchar(200) DEFAULT NULL,
+  `positionemployee` varchar(100) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `password`, `username`, `age`, `staff`, `company`, `address`, `phone`, `city`, `country`, `userInformation`, `postalCode`, `aboutMe`) VALUES
-(1, 'emanuel esteban', 'restrepo', 'e05072003@gmail.com', '$2b$10$/ZVcbLPSiVKSMj/', 'nabetse05', 21, 'empleado', 'Google', 'carrera #5 - 22', '3116686210', 'popayan', 'colombia','me gusta programar', 1456, 'ill be one the greatest'),
-(2, 'steiner', 'herrera', 'steinerherrera@gmail.c', '$2b$10$9QqzmhR/S.0mZXA', 'juegosSteiner', 18, 'empleado', 'Amazon', 'carrera #6 - 22', '302 6226935', 'cali', 'colombia', 'Me gusta la sandia', 6541, 'hello word im steiner');
+INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `password`, `username`, `age`, `staff`, `company`, `address`, `phone`, `city`, `country`, `userInformation`, `postalCode`, `aboutMe`, `positionemployee`, `id_admin`) VALUES
+(1, 'emanuel esteban', 'restrepo', 'e05072003@gmail.com', '$2b$10$/ZVcbLPSiVKSMj/', 'nabetse05', 21, 'desarrolador de soft', 'Siscontri Software', 'carrera #5 - 22', '3116686210', 'popayan', 'colombia', 'quiero crecer cada dia mas en esta empresa', 1456, 'ill be one the greatest', '', NULL),
+(2, 'steiner', 'herrera', 'steinerherrera@gmail.c', '$2b$10$9QqzmhR/S.0mZXA', 'juegosSteiner', 18, 'empleado', 'Amazon', 'carrera #6 - 22', '302 6226935', 'cali', 'colombia', 'Me gusta la sandia', 6541, 'hello word im steiner', '', NULL);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `employees`
---
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `employees_users_fk` (`id_user`);
 
 --
 -- Indexes for table `inventory`
@@ -145,12 +113,6 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `employees`
---
-ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
@@ -166,17 +128,11 @@ ALTER TABLE `spareparts`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `employees`
---
-ALTER TABLE `employees`
-  ADD CONSTRAINT `employees_users_fk` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `inventory`
