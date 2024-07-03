164d999cd930e519529c80917f7a8303f5e19fc8
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-07-03 18:36:44 -0500

    se integra nueva bd

--- Backend/bd/stocktaking.sql
+++ Backend/bd/stocktaking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2024 at 10:56 PM
+-- Generation Time: Jul 04, 2024 at 01:36 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -33,8 +33,20 @@ CREATE TABLE `inventory` (
   `value` decimal(10,0) DEFAULT NULL,
   `idUser` int(11) NOT NULL,
   `date` datetime DEFAULT NULL,
-  `saleDate` datetime DEFAULT NULL
+  `saleDate` datetime DEFAULT NULL,
+  `amount` int(11) NOT NULL DEFAULT 1,
+  `id_Admin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `sparePart`, `value`, `idUser`, `date`, `saleDate`, `amount`, `id_Admin`) VALUES
+(1, 4, 15000, 1, '2024-07-01 11:46:11', '2024-07-10 11:46:11', 1, 1),
+(3, 4, 17500, 19, '2024-07-19 00:00:00', '2024-07-31 00:00:00', 2, 18),
+(4, 4, 16000, 20, '2024-07-01 11:46:11', '2024-07-10 11:46:11', 15, 18),
+(5, 4, 17000, 18, '2024-07-01 16:46:11', '2024-07-10 16:46:11', 1, 18);
 
 -- --------------------------------------------------------
 
@@ -49,6 +61,14 @@ CREATE TABLE `spareparts` (
   `brand` varchar(200) DEFAULT NULL,
   `code` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Dumping data for table `spareparts`
+--
+
+INSERT INTO `spareparts` (`id`, `image`, `sparePart`, `brand`, `code`) VALUES
+(4, 'llave.png', 'llave', 'mazda', 158),
+(9, 'llave2.png', 'llave', 'renault', 123456);
 
 -- --------------------------------------------------------
 
@@ -83,9 +103,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `password`, `username`, `age`, `staff`, `company`, `address`, `phone`, `city`, `country`, `userInformation`, `postalCode`, `aboutMe`, `id_admin`) VALUES
 (1, 'emanuel esteban', 'restrepo', 'e05072003@gmail.com', '$2b$10$/ZVcbLPSiVKSMj/', 'nabetse05', 21, 'desarrolador de soft', 'Siscontri Software', 'carrera #5 - 22', '3116686210', 'popayan', 'colombia', 'quiero crecer cada dia mas en esta empresa', 1456, 'ill be one the greatest', NULL),
 (2, 'steiner', 'herrera', 'steinerherrera@gmail.c', '$2b$10$9QqzmhR/S.0mZXA', 'juegosSteiner', 18, 'empleado', 'Amazon', 'carrera #6 - 22', '302 6226935', 'cali', 'colombia', 'Me gusta la sandia', 6541, 'hello word im steiner', NULL),
-(15, 'ariana', 'Andrade', 'Andrade@gmail.com', '$2b$10$/ZVcbLPSiVKSMj/', 'arianitaHermosa', 16, 'asistente administra', 'asistente', 'carrera #5 - 22', '310 4295482', 'popayan', 'colombia', 'quiero crecer cada dia mas en esta empresa', 1456, 'persona con excelente carisma', 1),
 (16, 'miguel', 'pinto', 'pinto@gmail.com', '123456', 'pinto', 18, 'gerente financiero', 'webcol', 'carerra 4 #13-15', '325012145', 'cali', 'Colombia', 'te encargaras de manejar las finanzas y ser un excelente trabajador', 5623256, 'excelente persona, muy tranquilo y buen compañero', 1),
-(17, 'alex', 'zurdo', 'zurdo@gmail.com', '123456', 'zurdo', 21, 'mecanico', 'webcol', 'carerra 4 #15-16', '251455142504', 'Piendamó', 'Colombia', 'se encargara de cambiar llanatas', 52152, 'excelente en su trabajo, no se queda con lo primero', 1);
+(18, 'fernando', NULL, 'fernando@gmail.com', '$2b$10$TNYv5zxHt59C8Yu', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(19, '1111479211', 'herrera', 'pinto@gmail.com', '123456', 'pinto', 18, 'empacador', 'tubos y maderas', '3116686210', '3116686210', 'Piendamó', 'Colombia', 'es bueno', 190530, 'execlente', 18),
+(20, '1111479211', 'zurdo', 'zurdo@gmail.com', '', 'zurdo', 18, '1111479211', 'miguel', '3116686210', '3116686210', 'Piendamó', 'Colombia', 'ass', 190530, 'ass', 18);
 
 --
 -- Indexes for dumped tables
@@ -96,7 +117,8 @@ INSERT INTO `users` (`id`, `name`, `lastName`, `email`, `password`, `username`, 
 --
 ALTER TABLE `inventory`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `inventory_spareparts_fk` (`sparePart`);
+  ADD KEY `inventory_spareparts_fk` (`sparePart`),
+  ADD KEY `inventory_users_FK` (`idUser`);
 
 --
 -- Indexes for table `spareparts`
@@ -118,19 +140,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `spareparts`
 --
 ALTER TABLE `spareparts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
@@ -140,7 +162,8 @@ ALTER TABLE `users`
 -- Constraints for table `inventory`
 --
 ALTER TABLE `inventory`
-  ADD CONSTRAINT `inventory_spareparts_fk` FOREIGN KEY (`sparePart`) REFERENCES `spareparts` (`id`);
+  ADD CONSTRAINT `inventory_spareparts_fk` FOREIGN KEY (`sparePart`) REFERENCES `spareparts` (`id`),
+  ADD CONSTRAINT `inventory_users_FK` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
